55378cde599a6ef6632d4e02eb31494616d0e9fc
Akash Deep <65231036+akashhxthakur@users.noreply.github.com>
2023-03-07 14:33:50 +0530

    Users

--- App.js
+++ App.js
@@ -4,12 +4,46 @@ import ChatListItem from "./src/components/ChatListitem";
 import ChatsScreen from "./src/components/screens/ChatsScreen";
 import ChatScreen from "./src/components/screens/ChatScreen";
 import Navigator from "./src/navigation";
-import { Amplify } from "aws-amplify";
+import { Amplify, Auth, API, graphqlOperation } from "aws-amplify";
 import awsconfig from "./src/aws-exports";
 import { withAuthenticator } from "aws-amplify-react-native";
+import { getUser } from "./src/graphql/queries";
+import { createUser } from "./src/graphql/mutations";
+import { useEffect } from "react";
 
 Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 function App() {
+  useEffect(() => {
+    const syncUser = async () => {
+      //get Auth User
+      const authUser = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+      console.log(authUser);
+
+      // wuery the database using Auth user id (sub)
+      const userData = await API.graphql(
+        graphqlOperation(getUser, { id: authUser.attributes.sub })
+      );
+      console.log(userData);
+      if (userData.data.getUser) {
+        console.log("user already exists in database");
+        return;
+      }
+
+      //if there is no users in db, create one
+      const newUser = {
+        id: authUser.attributes.sub,
+        name: authUser.attributes.phone_number,
+        status: "Hey there! I'm new to whatsapp.",
+      };
+      console.log(newUser);
+      const newUserResponse = await API.graphql(
+        graphqlOperation(createUser, { input: newUser })
+      );
+    };
+    syncUser();
+  }, []);
   return (
     <View style={styles.container}>
       <Navigator />
