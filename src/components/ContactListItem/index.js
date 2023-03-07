f2905a19c6b89cac3c37f0b82d4517f96cf07a3d
Akash Deep <65231036+akashhxthakur@users.noreply.github.com>
2023-03-07 15:34:14 +0530

    Minor changes

--- src/components/ContactListItem/index.js
+++ src/components/ContactListItem/index.js
@@ -3,15 +3,46 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 import { useNavigation } from "@react-navigation/native";
+import { API, Auth, graphqlOperation } from "aws-amplify";
+import { createChatRoom, createUserChatRoom } from "../../graphql/mutations";
+
 const ContactListItem = ({ user }) => {
   const navigation = useNavigation();
+  const onPress = async () => {
+    console.log("Pressed!");
+
+    //Check if we already have a ChatRoom with user
+
+    //Create a new ChatRoom
+    const newChatRoomData = await API.graphql(
+      graphqlOperation(createChatRoom, { input: {} })
+    );
+    console.log(newChatRoomData);
+    if (!newChatRoomData.data?.createChatRoom) {
+      console.log("Error creating the chat error");
+    }
+    const newChatRoom = newChatRoomData.data?.createChatRoom;
+
+    //Add the clicked user to the ChatRoom
+    await API.graphql(
+      graphqlOperation(createUserChatRoom, {
+        input: { chatRoomID: newChatRoom.id, userID: user.id },
+      })
+    );
+
+    //Add the auth user to the ChatRoom
+    const authUser = await Auth.currentAuthenticatedUser();
+    await API.graphql(
+      graphqlOperation(createUserChatRoom, {
+        input: { chatRoomID: newChatRoom.id, userID: authUser.attributes.sub },
+      })
+    );
+
+    //Navigate to the newly created ChatRoom
+    navigation.navigate("Chat", { id: newChatRoom.id });
+  };
   return (
-    <Pressable
-      onPress={() =>
-        navigation.navigate("Chat", { id: chat.id, name: chat.user.name })
-      }
-      style={styles.container}
-    >
+    <Pressable onPress={onPress} style={styles.container}>
       <Image source={{ uri: user.image }} style={styles.image} />
       <View style={styles.content}>
         <Text numberOfLines={1} style={styles.name}>
